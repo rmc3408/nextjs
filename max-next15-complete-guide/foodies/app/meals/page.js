04b8853ffd17ac2d5cdc7d82cf04a7866814a562
rmc3408 <molinaro.raphael@gmail.com>
2025-07-04 16:52:21 -0400

    video 119 done

--- max-next15-complete-guide/foodies/app/meals/page.js
+++ max-next15-complete-guide/foodies/app/meals/page.js
@@ -1,18 +1,23 @@
-import MealGrid from "@/components/meals/meal-grid"
-import classes from "./page.module.css"
-import { getMeals } from "@/services/meal"
-import { Suspense } from "react"
-import MealLoader from "@/components/loading/loader"
+import Link from 'next/link'
+import MealGrid from '@/components/meals/meal-grid'
+import classes from './page.module.css'
+import { getMeals } from '@/services/meal'
+import { Suspense } from 'react'
+import MealLoader from '@/components/loading/loader'
 
 export default function MealsPage() {
   console.log('SSR Meals Grid Page')
-  
+
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals created by <span className={classes.highlight}>You</span>
+          Delicious meals, created <span className={classes.highlight}>by you</span>
         </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
       </header>
       <main>
         <Suspense fallback={<MealLoader />}>
